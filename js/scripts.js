3fc737a5a6bda67958a2aa186492e9d74e8d7aa2
Bradley Reed <shenmusha@yahoo.com>
2021-06-04 15:54:17 -0400

    added tables, cards, and an accordian

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(function() {
         $("#reserveModal").modal("toggle");
     });
     $(".carousel ").carousel({
-        interval: 3000
+        interval: 2500
     });
     $("#carouselButton").click(function() {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
@@ -20,6 +20,17 @@ $(function() {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+    $("#carouselButton1").click(function() {
+        if ($("#carouselButton1").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton1").children("i").removeClass("fa-pause");
+            $("#carouselButton1").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton1").children("i").removeClass("fa-play");
+            $("#carouselButton1").children("i").addClass("fa-pause");
         }
     });
 });
